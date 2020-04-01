99e45161b4984b7e8f995623c5f4c053c434f871
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-03-31 20:48:29 -0600

    Add main2 CRUD with lists

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # CRUD Python
 
-SPARTAN SALES proyect
+SPARTAN SALES CRUD
 
 ## Description
+This code contains a simple CRUD program
+* main.py CRUD based in Strings
+* main2.py CRUD based in lists
 
 ## How to Run
+
+For build main.py run this on terminal
+```bash
+python main.py
+```
+
+For build main2.py run this on terminal
+```bash
+python main2.py
+```
 
 ## Preview
---
+++ main2.py
@@ -0,0 +1,94 @@
+import sys
+
+clients = ['pablo','ricardo']
+
+def create_client(client_name):
+    global clients
+
+    if client_name not in clients:
+        clients.append(client_name)
+    else:
+        print('Client already is in the client\'s list')
+
+def list_clients():
+    for idx, client in enumerate(clients):
+        print(f'{idx}: {client}')
+
+def update_client(client_name, updated_client_name):
+    global clients
+    
+    if client_name in clients:
+        index = clients.index(client_name)
+        clients[index] = updated_client_name
+    else:
+        _not_client_exist_message()
+
+def delete_client(client_name):
+    global clients
+    
+    if client_name in clients:
+        clients.remove(client_name)
+    else:
+        _not_client_exist_message()
+
+def search_client(client_name):
+    for client in clients:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+def _not_client_exist_message():
+    print('Client is not in client\'s list')
+
+def _print_welcome():
+    print('WELCOME TO SPARTAN SALES')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[R]ead clients')
+    print('[U]pdate client')
+    print('[D]elete client')
+    print('[S]earch client')
+
+def _get_client_name():
+    client_name = None
+    while not client_name:
+        client_name = input('What is the client name? ')
+        if client_name == 'exit':
+            client_name = None
+            break
+    if not client_name:
+        sys.exit()
+
+    return client_name
+
+if __name__ == "__main__":
+    
+    _print_welcome()
+    command = input('Type option: ').upper()
+
+    if command == 'C':
+        client_name = _get_client_name()
+        create_client(client_name)
+        list_clients()
+    elif command == 'R':
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name? ')
+        update_client(client_name, updated_client_name)
+        list_clients()
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
+    else: 
+        print('Invalid command')
